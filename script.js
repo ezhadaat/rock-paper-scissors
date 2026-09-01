786aeeef7b0b71a103b73e95538afc5132b8cff3
ezhadaat <296503309+ezhadaat@users.noreply.github.com>
2026-09-01 18:19:36 +0100

    Write playRound function

--- script.js
+++ script.js
@@ -10,5 +10,52 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return prompt("Rock, paper or scissors?");
+    let choice = prompt("Rock, paper or scissors?");
+    return (choice.toLowerCase());
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound() {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    
+    console.log(humanChoice);
+    console.log(computerChoice);
+    if (humanChoice === computerChoice) {
+        console.log("Tie!");
+        return;
+    }
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            console.log("You win! Rock beats scissors!");
+            humanScore++;
+            return;
+        } else {
+            console.log("You lose! paper beats rock!");
+            computerScore++;
+            return;
+        }
+    }
+    if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper beats rock!");
+            humanScore++;
+            return;
+        }
+        console.log("You lose! Scissors beat paper!");
+        computerScore++;
+        return;
+    }
+    if (computerChoice === "rock") {
+        console.log("You lose! Rock beats scissors!");
+        computerScore++;
+        return;
+    }
+    console.log("You win! scissors beat paper!");
+    humanScore++;
+    return;
+}
+
+// playRound(humanChoice, computerChoice);
